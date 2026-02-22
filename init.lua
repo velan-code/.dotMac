89a80fa11718d064c0d445fa2af1a0105afbcdc5
Velan <228343634+velan-code@users.noreply.github.com>
2026-02-22 20:55:58 +0530

    Configure flutter-tools and set transparent background
    Added configuration for flutter-tools and made nvim transparent by setting background colors to none.

--- init.lua
+++ init.lua
@@ -2,9 +2,17 @@
 require("config.lazy")
 require("plugins.color")
 require("plugins.tmux")
+require("flutter-tools").setup({})
 
+-- make nvim transparent
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
+
+-- The main background of the tab line
+vim.api.nvim_set_hl(0, "TabLine", { bg = "none", fg = "white" })
+vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none", fg = "cyan", bold = true })
 
 vim.g.python3_host_prog = "/usr/bin/python3"
 
