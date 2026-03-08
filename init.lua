47a24c42e02b1219fa838c17f36032d155f6fb1f
Velan <228343634+velan-code@users.noreply.github.com>
2026-03-08 19:59:53 +0530

    Updating configs

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("flutter-tools").setup({})
+require("flutter-tools").setup({
+    widget_guides = { enabled = true },
+    decorations = { statusline = { device = true } },
+    lsp = { color = { enabled = true } },
+    dev_log = { enabled = true, open_cmd = "tabedit" },
+    hot_reload_on_save = true,
+})
 
 -- make nvim transparent
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -13,7 +19,6 @@ vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
 vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none", fg = "cyan", bold = true })
 
 vim.g.python3_host_prog = "/usr/bin/python3"
-
 --[[require("nvim-treesitter").setup({
   ensure_installed = { "python", "bash" },
   highlight = {
