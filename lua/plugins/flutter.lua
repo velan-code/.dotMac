f4c9267ebe1a8d45c4315ab74e03d5046f77490d
Velan <228343634+velan-code@users.noreply.github.com>
2026-03-20 17:23:19 +0530

    Updating

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,17 +1,27 @@
 -- Flutter for nvim plugin
 return {
-    "nvim-flutter/flutter-tools.nvim",
-    lazy = false,
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "stevearc/dressing.nvim", -- optional for vim.ui.select
-    },
-    config = function()
-        require("flutter-tools").setup({
-            widget_guides = { enabled = true },
-            decorations = { statusline = { device = true } },
-            lsp = { color = { enabled = true } },
-            hot_reload_on_save = true,
-        })
-    end,
+  "nvim-flutter/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim", -- optional for vim.ui.select
+  },
+  config = function()
+    require("flutter-tools").setup(
+      {
+        widget_guides = { enabled = true },
+        decorations = { statusline = { device = true } },
+        lsp = { color = { enabled = true } },
+        hot_reload_on_save = true,
+      },
+      require("lspconfig").dartls.setup({
+        settings = {
+          dart = {
+            completeFunctionCalls = false, -- Disables auto-insertion of () and params
+            showTodos = true,
+          },
+        },
+      })
+    )
+  end,
 }
