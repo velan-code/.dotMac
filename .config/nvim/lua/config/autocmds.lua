10af50ba451d12d62a0f8cb315177df3a9403d98
velan-code <senthil0velan@gmail.com>
2026-04-11 13:22:37 +0530

    improved trasparency in vim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,26 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Transparent off/on command
+local is_transparent = true
+vim.api.nvim_create_user_command("Transparency", function()
+	--Switch value for act as toggle
+	is_transparent = not is_transparent
+	require("gruvbox").setup({
+		transparent_mode = is_transparent,
+	})
+
+	vim.cmd("colorscheme gruvbox") -- Reload apply changes through CMD
+
+	-- tabLine Transparency
+	vim.api.nvim_set_hl(0, "TabLine", { bg = "none", fg = "white" })
+	vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none", fg = "cyan", bold = true })
+	-- Message the panel for to check mode
+	if is_transparent then
+		print("Transparency is on")
+	else
+		print("Transparency is off")
+	end
+end, {})
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -28,27 +28,4 @@ if ok then
 	})
 end
 
--- Transparent off/on command
-local is_transparent = true
-vim.api.nvim_create_user_command("Transparency", function()
-	--Switch value for act as toggle
-	is_transparent = not is_transparent
-	require("gruvbox").setup({
-		transparent_mode = is_transparent,
-	})
-
-	vim.cmd("colorscheme gruvbox") -- Reload apply changes through CMD
-
-	-- tabLine Transparency
-	vim.api.nvim_set_hl(0, "TabLine", { bg = "none", fg = "white" })
-	vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
-	vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none", fg = "cyan", bold = true })
-	-- Message the panel for to check mode
-	if is_transparent then
-		print("Transparency is on")
-	else
-		print("Transparency is off")
-	end
-end, {})
-
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show LSP hover information" })
